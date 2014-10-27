ea7641e156ed71ebec96e67d830520255d5210d7
catarak <ctarakajian@gmail.com>
2014-10-27 11:53:42 -0400

    adding compilation of sass and js

--- app/controllers/WitApp.rb
+++ app/controllers/WitApp.rb
@@ -1,5 +1,20 @@
 class WitApp < Sinatra::Base
 set :views, File.expand_path('../../views', __FILE__)
+register Sinatra::AssetPack
+  assets do
+    serve '/js', :from => '../assets/javascripts'
+    js :application, [
+      '/js/*.js',
+    ]
+    serve '/css', :from => '../assets/stylesheets'
+    css :application, [
+      '/css/*.css',
+      '/css/*.sass'
+     ]
+     js_compression :jsmin
+     css_compression :sass
+  end
+
   get '/' do
     @r = WitHandler.new.pull_profile
 
