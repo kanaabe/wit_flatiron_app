855ba560ce827ee5b9871cb261de6ae469cad2f1
kanaabe <kanakoabe5@gmail.com>
2014-10-27 10:36:33 -0400

    added twitter component

--- app/controllers/WitApp.rb
+++ app/controllers/WitApp.rb
@@ -1,9 +1,26 @@
 class WitApp < Sinatra::Base
 set :views, File.expand_path('../../views', __FILE__)
-  get '/' do
-    @r = WitHandler.new.pull_profile
 
+  get '/' do
+    wit = WitHandler.new
+    #i = wit.get_intent
+    i = "get_kana_abe_profile"
+    #profile = wit.pull_profile(i["outcomes"][0]["intent"])
+    profile = wit.pull_profile(i)
+
+    @name = profile.name
+    @github = profile.github
+    @twitter = profile.twitter
+    @linkedin = profile.linkedin
+    @biography = profile.biography
+
+    #@tweets = TWITTER.get_all_tweets("kanaabe")
+
+    @tweets = TWITTER.user_timeline(:user =>"kana_abe", :count=>15)
+
+    #inding.pry
     erb :'index.html'
   end
+
 
 end
